ff53b2d00ce30346973fba655ae56051bf41acf3
unknown <Administrator@KANINI-LTP-455.gisbizdev.gisbiz.chennai.com>
2021-11-30 15:42:18 +0530

    commit2

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminContext context;
+
+        public SqlStudentRepository(StudentAdminContext context)
+        {
+            this.context = context;
+        }
+
+        public StudentAdminContext Contect { get; }
+
+        public async Task<List<Student>> GetStudentsAsync()
+        {
+            return await context.Student.Include(nameof(Gender)).Include(nameof(Address)).ToListAsync();
+        }
+    }
+}
